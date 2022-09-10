df66bf705189f952686f02e024d7aa6ed83aa332
vladyslavsurin <vladsurin407@gmail.com>
2022-09-10 16:00:19 +0300

    add parse google data

--- src/controllers/forms.js
+++ src/controllers/forms.js
@@ -17,65 +17,167 @@ const addForms = catchError(async (req, res) => {
     }
 
     const forms = await formsService.getFormsByFormId(formId);
-    const formData = await formsService.getFormsData(formId, accessToken);
-    const data = [];
+    const googleForm = await formsService.getFormsData(formId, accessToken);
+    const formData = [];
 
-    for (const item of formData.items) {
+    [
+        'Hi',
+        `Welcome to ${googleForm?.info?.title}`,
+        `${googleForm?.info?.description}`,
+    ].forEach((item) => {
+        formData.push({
+            title: item,
+            itemId: getId(),
+            type: 'simpleQuestion',
+        })
+    })
+
+    formData.push({
+        itemId: getId(),
+        required: false,
+        type: 'signQuestion',
+    })
+
+    googleForm?.items?.forEach((item, index) => {
+        if(index === 1) {
+            formData.push({
+                title: `Question ${index+1} out of ${googleForm?.items?.length}`,
+                itemId: getId(),
+                type: 'simpleQuestion',
+            })
+        }
+
+        if(index+1 === googleForm?.items?.length) {
+            formData.push({
+                title: 'Last question',
+                itemId: getId(),
+                type: 'simpleQuestion',
+            })
+        }
+
+            formData.push({
+            title: item.title,
+            itemId: item.itemId,
+            type: 'simpleQuestion',
+        })
+
+        if(item?.description) {
+            formData.push({
+                title: item?.description,
+                itemId: getId(),
+                type: 'simpleQuestion',
+            })
+        }
+
         if(item?.questionGroupItem) {
             item?.questionGroupItem?.questions.map((question) => {
-                data.push({
-                    itemId: getId(),
+                formData.push({
                     title: question?.rowQuestion?.title,
-                    questionItem: {
-                        question: {
-                            choiceQuestion: {
-                                options: item?.questionGroupItem?.grid?.columns?.options?.map((option) => ({
-                                    ...option,
-                                    itemId: getId(),
-                                    checked: false,
-                                })),
-                                type: item?.questionGroupItem?.grid?.columns?.type
-                            }
-                        },
-                        questionId: question?.questionId,
-                        required: question?.required
-                    }
+                    itemId: getId(),
+                    type: 'simpleQuestion',
+                })
+                formData.push({
+                    questions: item?.questionGroupItem?.grid?.columns?.options?.map((option) => ({
+                        ...option,
+                        questionId: getId(),
+                        checked: false,
+                    })),
+                    questionsType: item?.questionGroupItem?.grid?.columns?.type,
+                    itemId: question?.questionId,
+                    required: !question?.required,
+                    type: 'choiceQuestion',
                 })
             })
+            return;
         }
 
         if(item?.questionItem?.question?.choiceQuestion) {
-            data.push({
-                ...item,
-                questionItem: {
-                    question: {
-                        choiceQuestion: {
-                            ...item?.questionItem?.question?.choiceQuestion,
-                            options: item?.questionItem?.question?.choiceQuestion?.options?.map((option) => ({
-                                ...option,
-                                itemId: getId(),
-                                checked: false,
-                            })),
-                        }
-                    },
-                }
+            formData.push({
+                itemId: item?.questionItem?.question?.questionId,
+                required: !item?.questionItem?.question?.required,
+                questions: item?.questionItem?.question?.choiceQuestion?.options?.map((option) => ({
+                    ...option,
+                    questionId: getId(),
+                    checked: false,
+                })),
+                questionsType: item?.questionItem?.question?.choiceQuestion?.type,
+                type: 'choiceQuestion',
             })
         }
 
-        if(!item?.questionGroupItem) {
-            data.push(item)
+        if(item?.questionItem?.question?.scaleQuestion) {
+            formData.push({
+                title: `${item?.questionItem?.question?.scaleQuestion?.low} ${item?.questionItem?.question?.scaleQuestion?.lowLabel}`,
+                itemId: getId(),
+                type: 'simpleQuestion',
+            })
+            formData.push({
+                title: `${item?.questionItem?.question?.scaleQuestion?.high} ${item?.questionItem?.question?.scaleQuestion?.highLabel}`,
+                itemId: getId(),
+                type: 'simpleQuestion',
+            })
+            formData.push({
+                itemId: item?.questionItem?.question?.questionId,
+                required: !item?.questionItem?.question?.required,
+                options: {
+                    ...item?.questionItem?.question?.scaleQuestion
+                },
+                type: 'scaleQuestion',
+            })
         }
-    }
+
+        if(item?.questionItem?.question?.textQuestion) {
+            formData.push({
+                itemId: item?.questionItem?.question?.questionId,
+                required: !item?.questionItem?.question?.required,
+                type: 'textQuestion',
+            })
+        }
+
+        if(item?.questionItem?.question?.dateQuestion) {
+            formData.push({
+                itemId: item?.questionItem?.question?.questionId,
+                required: !item?.questionItem?.question?.required,
+                options: {
+                    ...item?.questionItem?.question?.dateQuestion
+                },
+                type: 'dateQuestion',
+            })
+        }
+
+        if(item?.questionItem?.question?.timeQuestion) {
+            formData.push({
+                itemId: item?.questionItem?.question?.questionId,
+                required: !item?.questionItem?.question?.required,
+                options: {
+                    ...item?.questionItem?.question?.timeQuestion
+                },
+                type: 'timeQuestion',
+            })
+        }
+    })
+
+    formData.push({
+        itemId: getId(),
+        required: false,
+        type: 'submitQuestion',
+    })
+
+    formData.push({
+        title: 'Thank you!',
+        itemId: getId(),
+        type: 'simpleQuestion',
+    })
 
     // const imageUri = await formsService.getFormsImage(formData?.responderUri);
 
     if (forms) {
-        await formsService.updateForms(formId, {...formData, items: data});
+        await formsService.updateForms(formId, {...googleForm, items: formData});
         return res.status(httpStatus.CREATED).json(forms.id);
     }
 
     // imageUri: imageUri
-    const newForms = await formsService.addForms({...formData, items: data});
+    const newForms = await formsService.addForms({...googleForm, items: formData});
     return res.status(httpStatus.CREATED).json(newForms.id);
 });
 
